32f4cc550aa0900f65b021dc10e7536930b182d5
Mathw7 <matheusgtescola@gmail.com>
2023-02-10 14:10:40 -0300

    Inserindo links

--- src/components/pages/Contato.js
+++ src/components/pages/Contato.js
@@ -2,6 +2,8 @@ import React from "react";
 import '../styles/Contato.css';
 import Header from "../header";
 import { FaLinkedin, FaEnvelope, FaGithub, FaWhatsapp } from "react-icons/fa";
+import {BrowserRouter, Link } from 'react-router-dom';
+
 export default (props)=>(
     
     <>
@@ -10,18 +12,18 @@ export default (props)=>(
         <div id="container_redes">
           <h2 id="text_siga">Siga-nos</h2>
           <ul>
-            <li className="List"><FaLinkedin className="icons"/></li>
-            <li className="List"><FaEnvelope className="icons"/></li>
-            <li className="List"><FaGithub className="icons"/></li>
-            <li className="List"><FaWhatsapp className="icons"/></li>
+            <li className="List"><Link to="https://www.linkedin.com/in/matheus-rozario-702271244"><FaLinkedin className="icons"/></Link></li>
+            <li className="List"><Link to="mailto:matheusgttrabalho@gmail.com"><FaEnvelope className="icons"/></Link></li>
+            <li className="List"><Link to="https://github.com/Mathwz7/"><FaGithub className="icons"/></Link></li>
+            <li className="List"><Link to="tel:5511974312473"><FaWhatsapp className="icons"/></Link></li>
           </ul>
         </div>
         <div id="container_form">
           <h1 id="title_form">Formulario de Contato</h1>
           <form method="GET" action="contato" id="form">
-            <input type="text" placeholder="Seu nome" className="input_text"/>
-            <input type="email" placeholder="Seu email" className="input_text"/>
-            <input type="text" placeholder="Sua mensagem" id="input_mensagem"/>
+            <input type="text" placeholder="Seu nome" className="input_text" required/>
+            <input type="email" placeholder="Seu email" className="input_text" required/>
+            <input type="text" placeholder="Sua mensagem" id="input_mensagem" required/>
             <input type="submit" value="Enviar" name="enviar" id="btn"/>
           </form>
         </div>
--- src/components/pages/Projetos.js
+++ src/components/pages/Projetos.js
@@ -1,14 +1,17 @@
 import React from "react";
 import '../styles/Projetos.css';
 import Header from "../header";
+import {BrowserRouter, Link } from 'react-router-dom';
 export default (props)=>(
     
     <>
       <Header></Header>
       <main className="main">
         
-        <div className="container_projetos">
-        </div>
+        <Link to="https://mathwz7.github.io/Gerador_de_senha/">
+          <div className="container_projetos" id="projeto1">
+          </div>
+        </Link>
         <div className="container_projetos">
         </div>
         <div className="container_projetos">
